6e70a208b2a015c95fbd9b8d687bc398973d7a47
DynnG <geraldyncataluna2nd@gmail.com>
2025-12-01 23:24:07 +0800

    removed db error

--- app/src/main/java/com/example/clotheorganizer/Category.kt
+++ app/src/main/java/com/example/clotheorganizer/Category.kt
@@ -3,14 +3,4 @@ package com.example.clotheorganizer
 class Category(
     var categoryID: Int? = null,      // PK
     var categoryName: String? = null  // Tops, Accessories, etc
-) {
-    fun getCategoryID(): Int? = categoryID
-    fun setCategoryID(id: Int) {
-        categoryID = id
-    }
-
-    fun getCategoryName(): String? = categoryName
-    fun setCategoryName(name: String) {
-        categoryName = name
-    }
-}
+)
--- app/src/main/java/com/example/clotheorganizer/User.kt
+++ app/src/main/java/com/example/clotheorganizer/User.kt
@@ -6,29 +6,4 @@ class User(
     var password: String? = null,
     var email: String? = null,
     var fullname: String? = null
-) {
-    fun getUserID(): Int? = userID
-    fun setUserID(id: Int) {
-        userID = id
-    }
-
-    fun getUserName(): String? = userName
-    fun setUserName(name: String) {
-        userName = name
-    }
-
-    fun getPassword(): String? = password
-    fun setPassword(pass: String) {
-        password = pass
-    }
-
-    fun getEmail(): String? = email
-    fun setEmail(mail: String) {
-        email = mail
-    }
-
-    fun getFullname(): String? = fullname
-    fun setFullname(name: String) {
-        fullname = name
-    }
-}
+)
